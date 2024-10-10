889cd8eac4d8d52349aaff298102d1cfab5bc083
KaiN <kain@piwnica.ws>
2024-10-10 23:14:28 +0200

    generate windows icons for console-enabled executable

--- platform/windows/make_icons.py
+++ platform/windows/make_icons.py
@@ -20,6 +20,7 @@ class badge_descriptor:
     path: str
     x: int
     y: int
+    pos_reverse: bool = False
     width: int | None = None
     height: int | None = None
 
@@ -40,16 +41,24 @@ def load_svg(path: str, width=None, height=None) -> SVGSurface:
 def assemble_icon_image(icon_path: str, icon_size: int, badges: list[badge_descriptor]) -> ImageFile:
     icon_png = Image.open(io.BytesIO(load_svg(icon_path, height=icon_size).cairo.write_to_png(target=None)))
 
-    for badge_desc in badges:
+    for badge_descriptor in badges:
         badge_png = Image.open(
-            io.BytesIO(load_svg(badge_desc.path, height=badge_desc.height).cairo.write_to_png(target=None))
+            io.BytesIO(load_svg(badge_descriptor.path, height=badge_descriptor.height).cairo.write_to_png(target=None))
         )
 
-        icon_png.alpha_composite(badge_png, (badge_desc.x, badge_desc.y))
+        badge_pos = (
+            (
+                icon_png.width - badge_png.width - badge_descriptor.x,
+                icon_png.height - badge_png.height - badge_descriptor.y,
+            )
+            if badge_descriptor.pos_reverse
+            else (badge_descriptor.x, badge_descriptor.y)
+        )
+        icon_png.alpha_composite(badge_png, badge_pos)
     return icon_png
 
 
-def compose_windows_icon(build_kind: str, out_path: str):
+def compose_windows_icon(build_kind: str, out_path: str, is_console: bool = False):
     base_icon_path = "../../icon.svg"
 
     sizes: list[int] = [256, 128, 64, 48, 32, 16]
@@ -66,6 +75,16 @@ def compose_windows_icon(build_kind: str, out_path: str):
         icon_badges[32].append(badge_descriptor(build_kind_badge_svg, x=0, y=0, height=11))
         icon_badges[16].append(badge_descriptor(build_kind_badge_mini_svg, x=0, y=0, height=6))
 
+    if is_console:
+        console_badge_svg = "icon_badges/console.svg"
+
+        icon_badges[256].append(badge_descriptor(console_badge_svg, x=10, y=10, height=80, pos_reverse=True))
+        icon_badges[128].append(badge_descriptor(console_badge_svg, x=10, y=10, height=40, pos_reverse=True))
+        icon_badges[64].append(badge_descriptor(console_badge_svg, x=0, y=0, height=24, pos_reverse=True))
+        icon_badges[48].append(badge_descriptor(console_badge_svg, x=0, y=0, height=17, pos_reverse=True))
+        icon_badges[32].append(badge_descriptor(console_badge_svg, x=0, y=0, height=13, pos_reverse=True))
+        icon_badges[16].append(badge_descriptor(console_badge_svg, x=0, y=0, height=8, pos_reverse=True))
+
     images: list[ImageFile] = []
     for size, badges in icon_badges.items():
         image = assemble_icon_image(base_icon_path, size, badges)
@@ -80,3 +99,4 @@ def compose_windows_icon(build_kind: str, out_path: str):
 
 for build_kind in ["beta", "rc", "dev", "stable"]:
     compose_windows_icon(build_kind, f"icons/godot_{build_kind}.ico")
+    compose_windows_icon(build_kind, f"icons/godot_console_{build_kind}.ico", is_console=True)
